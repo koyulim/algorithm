afbb6a71b38f37c441bba67b5cc77d6dbbc785fa
koyulim <89690794+koyulim@users.noreply.github.com>
2024-09-08 15:22:23 +0900

    [Silver V] Title: 나이순 정렬, Time: 652 ms, Memory: 58528 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -7,23 +7,24 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int size = Integer.parseInt(br.readLine());
         HashMap<Integer, ArrayList<String>> map = new HashMap<>();
+        ArrayList<String> list = new ArrayList<String>();
 
         for (int i = 0; i < size; i++) {
             String[] input = br.readLine().split(" ");
             int age = Integer.parseInt(input[0]);
             String name = input[1];
-            ArrayList<String> list = new ArrayList<String>();
+            list = new ArrayList<String>();
+            
             if (map.containsKey(age)) {
                 list = map.get(age);
                 list.add(name);
             } else {
                 list.add(name);
             }
-            map.put(age,list);
+            map.put(age, list);
         }
 
         for (int key : map.keySet()) {
-            ArrayList<String> list = new ArrayList<String>();
             list = map.get(key);
             for (int i = 0; i < list.size(); i++) {
                 bw.write(key + " " + list.get(i) + "\n");

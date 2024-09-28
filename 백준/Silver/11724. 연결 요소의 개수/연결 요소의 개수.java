074ae4a6683aecb4378fea25c06fd902dd6a65cb
koyulim <89690794+koyulim@users.noreply.github.com>
2024-09-28 18:05:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 644 ms, Memory: 160080 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Point;
+
+public class Main {
+
+    static class Node {
+        int n;
+        List<Node> neighbor;
+        boolean visit;
+        Node(int n) {
+            this.n = n;
+            neighbor = new ArrayList<>();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split(" ");        
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        Node[] nodes = new Node[n + 1];
+        for (int i = 0; i <= n; i++) {
+            nodes[i] = new Node(i);
+        }
+
+        for (int i = 0; i < m; i++) {
+            String[] input2 = br.readLine().split(" ");
+            int a = Integer.parseInt(input2[0]);
+            int b = Integer.parseInt(input2[1]);
+
+            nodes[a].neighbor.add(nodes[b]);
+            nodes[b].neighbor.add(nodes[a]);
+        }
+
+        int cnt = 0;
+        for (int j = 1; j <= n; j++) {
+            if (!nodes[j].visit) {
+                cnt++;
+                Queue<Node> queue = new LinkedList<>();
+                queue.add(nodes[j]);
+                nodes[j].visit = true;
+                while (!queue.isEmpty()) {
+                    int node = queue.poll().n;
+                    for (Node conect : nodes[node].neighbor) {
+                        if (!conect.visit) {
+                            queue.add(conect);
+                            nodes[conect.n].visit = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        bw.write(cnt + "\n");
+        bw.flush();
+    }
+}

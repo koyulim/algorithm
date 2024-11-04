29ddb33dd4fd580bf80a405bcbe42d024164cfda
koyulim <89690794+koyulim@users.noreply.github.com>
2024-11-04 21:27:16 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("|\\_/|" + "\n");
+        bw.write("|q p|   /}" + "\n");
+        bw.write("( 0 )\"\"\"\\" + "\n");
+        bw.write("|\"^\"`    |" + "\n");
+        bw.write("||_/=\\\\__|" + "\n");
+        bw.flush();
+    }
+}

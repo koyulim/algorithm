97b73e4e168a2e722eab1df5b5b016842db82153
koyulim <89690794+koyulim@users.noreply.github.com>
2024-12-09 20:33:54 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 116 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Point;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        
+        int cnt = 0;
+        for (int i = 5; i <= n; i*= 5) {
+            cnt += n / i;
+        }
+
+        bw.write(cnt + "\n");
+        bw.flush();
+    }
+}

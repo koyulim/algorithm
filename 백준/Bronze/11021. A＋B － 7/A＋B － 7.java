8e27384b1f9b0e4b090e8ce96b50a83e3b2b64d0
koyulim <89690794+koyulim@users.noreply.github.com>
2024-12-16 22:19:07 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 16220 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Point;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i ++) {
+            String[] input = br.readLine().split(" ");
+            bw.write("Case #"+ (i + 1) + ": " + (Integer.parseInt(input[0]) + Integer.parseInt(input[1])) + "\n" );
+        }
+
+        bw.flush();
+    }
+}

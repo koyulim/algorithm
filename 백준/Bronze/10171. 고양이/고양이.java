dd3d121692844a80e8c8607cfaa59989dcf65b0e
koyulim <89690794+koyulim@users.noreply.github.com>
2024-12-26 20:47:53 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("\\    /\\" + "\n");
+        bw.write(" )  ( ')" + "\n");
+        bw.write("(  /  )" + "\n");
+        bw.write(" \\(__)|" + "\n");
+        bw.close();
+    }
+}

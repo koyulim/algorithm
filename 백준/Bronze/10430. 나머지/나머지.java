26b826803af19444404121b7cd9dbe8b4506f4f6
koyulim <89690794+koyulim@users.noreply.github.com>
2025-01-08 20:46:10 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+import org.w3c.dom.Node;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] input = br.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int C = Integer.parseInt(input[2]);
+
+        bw.write(((A+B)%C) + "\n");
+        bw.write((((A%C) + (B%C))%C) + "\n");
+        bw.write(((A*B)%C) + "\n");
+        bw.write((((A%C) * (B%C))%C) + "\n");
+        bw.close();
+    }
+}

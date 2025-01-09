0467d83fe4ee235392f5d781cc1eee1df8393dc6
koyulim <89690794+koyulim@users.noreply.github.com>
2025-01-09 20:26:22 +0900

    [Bronze V] Title: We love kriii, Time: 96 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import org.w3c.dom.Node;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("강한친구 대한육군" + "\n");
+        bw.write("강한친구 대한육군" + "\n");
+        bw.close();
+    }
+}

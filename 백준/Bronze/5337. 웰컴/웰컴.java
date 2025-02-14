0ca2de5e525c03da7cf537e42f4df93ecf1e6da2
koyulim <89690794+koyulim@users.noreply.github.com>
2025-02-14 20:50:48 +0900

    [Bronze V] Title: 웰컴, Time: 100 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(".  .   ." + "\n");
+        bw.write("|  | _ | _. _ ._ _  _" + "\n");
+        bw.write("|/\\|(/.|(_.(_)[ | )(/." + "\n");
+        bw.flush();
+    }
+}

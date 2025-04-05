0bcacee6f21c2fa74d0228f23bd6e4f424066527
koyulim <89690794+koyulim@users.noreply.github.com>
2025-04-05 16:57:28 +0900

    Update 나이순 정렬.java

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,34 +1,28 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int size = Integer.parseInt(br.readLine());
-        HashMap<Integer, ArrayList<String>> map = new HashMap<>();
-        ArrayList<String> list = new ArrayList<String>();
 
+        List<String[]> list = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             String[] input = br.readLine().split(" ");
-            int age = Integer.parseInt(input[0]);
-            String name = input[1];
-            list = new ArrayList<String>();
-            
-            if (map.containsKey(age)) {
-                list = map.get(age);
-                list.add(name);
-            } else {
-                list.add(name);
-            }
-            map.put(age, list);
+            list.add(new String[]{input[0], input[1], String.valueOf(i)});
         }
 
-        for (int key : map.keySet()) {
-            list = map.get(key);
-            for (int i = 0; i < list.size(); i++) {
-                bw.write(key + " " + list.get(i) + "\n");
+        Collections.sort(list, (a, b) -> {
+            int ageCompare = Integer.compare(Integer.parseInt(a[0]), Integer.parseInt(b[0]));
+            if (ageCompare == 0) {
+                return Integer.compare(Integer.parseInt(a[2]), Integer.parseInt(b[2]));
             }
+            return ageCompare;
+        });
+
+        for (String[] person : list) {
+            bw.write(person[0] + " " + person[1] + "\n");
         }
         bw.flush();
     }

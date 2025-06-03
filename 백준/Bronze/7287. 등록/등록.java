ec9a586ceabb3a3302175f4c09b23e559eb47b06
koyulim <89690794+koyulim@users.noreply.github.com>
2025-06-03 09:55:07 +0900

    [Bronze V] Title: 등록, Time: 100 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("255" + "\n");
+        bw.write("kul2862" + "\n");
+        bw.flush();
+    }
+}
